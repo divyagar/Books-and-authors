3784780256e1edecab755b0ce42048d4e42d5af1
Divya garg <divyagarg2601@gmail.com>
2021-05-20 16:24:12 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    width: {
+      '300': '300px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  width: {
+    '300': '300px',
+  },
+}
