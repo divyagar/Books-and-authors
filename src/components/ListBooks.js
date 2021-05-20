20eaa46774b38a437f57e4d5f6ddcf1631a5a451
Divya garg <divyagarg2601@gmail.com>
2021-05-20 17:35:10 +0530

    Created option to delete book

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,29 +1,69 @@
 import React from 'react'
-import { Query } from 'react-apollo'
-import { getBooks } from './queries'
-
-const Data = () => (
-	<Query query={getBooks}>
-		{
-			({ loading, error, data }) => {
-				if (loading) return <p>Loading...</p>
-				if (error) return <p>Error while fetching data</p>
-				return (
-					<React.Fragment>
-						{
-							data.books.map(book => <div key={book.id}>{book.name}</div>)
-						}
-					</React.Fragment>
-				)
-			}
-		}
-	</Query>
-)
+import { Query, Mutation } from 'react-apollo'
+import { getBooks, deleteBook } from './queries'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 function ListBooks() {
 	return (
 		<div>
-			<Data />
+			<Query query={getBooks}>
+				{
+					({ loading, error, data }) => {
+						console.log(loading, error, data)
+						if (loading) return <p>Loading...</p>
+						if (error) return <p>Error while fetching data</p>
+						return (
+							<React.Fragment>
+								{
+									data.books.length > 0 &&
+									data.books.map(book =>
+										<div key={book.id} className="border-t-2 border-b-2 px-10 py-5 text-left">
+											<div>
+												<Mutation mutation={deleteBook} refetchQueries={() => {
+													return [{
+														query: getBooks
+													}]
+												}}>
+													{
+														(deleteBook, { loading, error, data }) => (
+															<FontAwesomeIcon
+																icon={faTrashAlt}
+																className="cursor-pointer"
+																color="red"
+																onClick={e => {
+																	e.preventDefault()
+																	deleteBook({
+																		variables: {
+																			id: book.id
+																		}
+																	})
+																}}
+															/>)
+													}
+												</Mutation>
+
+											</div>
+											<div>
+												<b>Name </b>: {book.name}
+											</div>
+											<div>
+												<b>Price</b> : {book.price}
+											</div>
+											<div>
+												<b>Author</b> : {book.author.name}
+											</div>
+										</div>
+									)
+								}
+								{
+									data.books.length == 0 && <p>No data to show</p>
+								}
+							</React.Fragment>
+						)
+					}
+				}
+			</Query>
 		</div>
 	)
 }
